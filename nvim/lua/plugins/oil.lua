dc73c0bf0c1b3135f4b3c5299a6aaa987865df46
Wladimir Urzúa Calderón <wladimir2000@protonmail.com>
2026-08-16 17:36:40 -0400

    feat(nvim): add yank_entry keymap to oil.nvim

--- nvim/lua/plugins/oil.lua
+++ nvim/lua/plugins/oil.lua
@@ -1,16 +1,22 @@
 return {
-  'stevearc/oil.nvim',
-  keys = {"-"},
-  -- Optional dependencies
-  dependencies = { "nvim-tree/nvim-web-devicons" },
-  config = function()
-    require('oil').setup({
-      view_options = {
-        -- Show files and directories that start with "."
-        show_hidden = true,
-      },
-    })
+	"stevearc/oil.nvim",
+	keys = { "-" },
+	-- Optional dependencies
+	dependencies = { "nvim-tree/nvim-web-devicons" },
+	config = function()
+		require("oil").setup({
+			view_options = {
+				-- Show files and directories that start with "."
+				show_hidden = true,
+			},
+			-- Se fusionan con los keymaps por defecto de oil.
+			keymaps = {
+				-- Copia el path del archivo/carpeta bajo el cursor al registro
+				-- (con clipboard=unnamedplus va también al portapapeles del sistema).
+				["gy"] = "actions.yank_entry", -- path absoluto
+			},
+		})
 
-    vim.keymap.set("n", "-", "<CMD>Oil --float<CR>", { desc = "Open parent directory" })
-  end,
+		vim.keymap.set("n", "-", "<CMD>Oil --float<CR>", { desc = "Open parent directory" })
+	end,
 }
