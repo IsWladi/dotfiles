deed59c50f0dcc11df63c558b7fd9ecaef437418
Wladimir Urzúa Calderón <wladimir2000@protonmail.com>
2026-09-03 19:37:08 -0400

    feat(nvim): lock specific spectre version

--- nvim/lua/plugins/spectre.lua
+++ nvim/lua/plugins/spectre.lua
@@ -1,5 +1,7 @@
 return {
 	"nvim-pack/nvim-spectre",
+	commit = "72f56f7585903cd7bf92c665351aa585e150af0f",
+	branch = "master",
 	cmd = "Spectre",
 	dependencies = { "nvim-lua/plenary.nvim" },
 	keys = {
